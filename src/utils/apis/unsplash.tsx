8333bbb545bbb1cd2c21aa462e004cf4d1b3ee38
RISHAV DAS <rishav510@Rishavs-MacBook-Pro.local>
2021-08-20 13:32:41 +0530

    key change

--- src/utils/apis/unsplash.tsx
+++ src/utils/apis/unsplash.tsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 const unsplash = axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID 8pSoXHrTXCPo9nTwA3KYJoW0tIq_9jsJRKYb81w5bFQ",
+    Authorization: "Client-ID mU4gT4_g5HHESerLMgNg7HWmM5Up3-U3KyqOVSVX9Qk",
   }
 })
 
